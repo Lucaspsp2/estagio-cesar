142324cec628d18f8109e81dd75aa9d7a2d772f8
LPSP <lpsp@EU314602016234.local>
2025-09-22 11:55:47 -0300

    Adicionando arquivos do Day 3

--- Day1/test_home_page.py
+++ Day1/test_home_page.py
@@ -1,11 +1,6 @@
 # test_demo.py
-from selenium import webdriver
+def test_verify_demoqa_title(driver):
+ driver.get("https://demoqa.com")
+ assert "DEMOQA" in driver.title
 
-def test_verify_demoqa_title():
-    driver = webdriver.Chrome()
-
-    driver.get("https://demoqa.com")
-
-    assert "DEMOQA" in driver.title
-
-    driver.quit()
+    
